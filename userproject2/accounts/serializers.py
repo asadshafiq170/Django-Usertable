e1f33092530356840920e9488aa8cf52f6cd0f1b
Asad Hussain <asadshafiq170@gmail.com>
2025-10-10 15:11:02 +0500

    Description of changes

--- userproject2/accounts/serializers.py
+++ userproject2/accounts/serializers.py
@@ -3,8 +3,8 @@ from django.contrib.auth import authenticate
 from .models import CustomUser
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
+class UserSerializer(serializers.ModelSerializer): # Serializer for CustomUser model
+    class Meta:  # Meta class to define model and fields
         model = CustomUser
         fields = (
             "id", "username", "email", "first_name", "last_name",
@@ -14,13 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ("full_name", "is_adult")   # ensure these are not editable
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):  # Serializer for user registration
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'password', 'date_of_birth')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
+    def create(self, validated_data):  # Create user with hashed password
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
@@ -29,11 +29,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
 
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.Serializer):   # Serializer for user login
     username = serializers.CharField()
     password = serializers.CharField()
 
-    def validate(self, data):
+    def validate(self, data): # Validate user credentials
         user = authenticate(username=data.get("username"), password=data.get("password"))
         if not user or not user.is_active:
             raise serializers.ValidationError("Invalid credentials")
